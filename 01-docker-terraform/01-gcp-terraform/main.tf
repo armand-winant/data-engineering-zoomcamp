071c22a249d693b210e0cd4bda414038c3a79523
Armand <armandwinant@googlemail.com>
2024-05-07 23:48:25 +0200

    add BQ dataset in terraform

--- 01-docker-terraform/01-gcp-terraform/main.tf
+++ 01-docker-terraform/01-gcp-terraform/main.tf
@@ -9,6 +9,28 @@ terraform {
 
 provider "google" {
   credentials = ".keys/my-creds.json"
-  project = "astral-pursuit-422621-e6"
-  region  = "europe-west3"
+  project     = "astral-pursuit-422621-e6"
+  region      = "europe-west3"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "astral-pursuit-422621-e6-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id    = "demo_dataset"
+  friendly_name = "test"
+  description   = "This is a test description"
+  location      = "EU"
 }
