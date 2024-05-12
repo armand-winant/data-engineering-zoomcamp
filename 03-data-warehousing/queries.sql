fa59ca0e89d09f1c6270d3af3ea1a20cfc310e0e
Armand <armandwinant@googlemail.com>
2024-05-12 23:28:41 +0200

    add query for creating clustered tables

--- 03-data-warehousing/queries.sql
+++ 03-data-warehousing/queries.sql
@@ -15,8 +15,26 @@ SELECT * FROM projectID.dataSet.tableName LIMIT 10;
 -- data is partitioned by the selected columns
 -- BigQuery will be able to compute the size/rows of the table
 
+-- partitioning can be done on different data types: time-unit, integer range
+-- partitioning should split the data evenly
+
 -- create a partitioned table
 CREATE OR REPLACE TABLE `projectID.dataSet.tableName_partitioned`
 PARTITION BY
   DATE(date_column) AS
 SELECT * FROM projectID.dataSet.tableName;
+
+-- CLUSTERING
+-- clustering can further improve cost and query performance
+-- clustering order is important as it determines the sorting of the data
+
+CREATE OR REPLACE TABLE `projectID.dataSet.tableName_partitioned`
+PARTITION BY DATE(date_column)
+CLUSTER BY clusteringColumn AS
+SELECT * FROM projectID.dataSet.tableName;
+
+
+-- the cost benefits of clustering are unknown (unlike partitioning)
+-- use clustering when more granularity is required (filtering on multiple columns)
+-- use clustering over partitioning when the oartition results in a small amount of data per partition
+-- use clustering over partitioning when partitions are modified frequently
