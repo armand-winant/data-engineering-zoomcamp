f0cfcd9c43babe97b599bc7b8d51b781d7cd16a6
Armand <armandwinant@googlemail.com>
2024-05-12 22:38:31 +0200

    add query for creating partitioned tables

--- 03-data-warehousing/queries.sql
+++ 03-data-warehousing/queries.sql
@@ -1,6 +1,22 @@
 -- CREATE A NEW EXTERNAL TABLE FROM GCS DATA
-CREATE OR REPLACE EXTERNAL TABLE 'projectID.dataSet.tableName'
+-- BigQuery is unable to determine the size/rows of an external table because the data iteself is not inside BigQuery
+
+CREATE OR REPLACE EXTERNAL TABLE `projectID.dataSet.tableName`
 OPTIONS (
   format = 'CSV',
   uris = ['gs://bucketName/folderName/files_*.csv']
-)
+);
+
+SELECT * FROM projectID.dataSet.tableName LIMIT 10;
+
+-- PARTITIONING
+-- partitioning can improve query performance by reducing amount of data scanned
+-- certain columns are often used for filtering
+-- data is partitioned by the selected columns
+-- BigQuery will be able to compute the size/rows of the table
+
+-- create a partitioned table
+CREATE OR REPLACE TABLE `projectID.dataSet.tableName_partitioned`
+PARTITION BY
+  DATE(date_column) AS
+SELECT * FROM projectID.dataSet.tableName;
