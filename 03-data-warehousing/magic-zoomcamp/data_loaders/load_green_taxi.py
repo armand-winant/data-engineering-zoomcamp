dd6b7d5dd2403542e726fe067cee3dc5f2936c1f
Armand <armandwinant@googlemail.com>
2024-05-13 10:44:23 +0200

    create pipeline files for green taxi etl

---
+++ 03-data-warehousing/magic-zoomcamp/data_loaders/load_green_taxi.py
@@ -0,0 +1,26 @@
+import io
+import pandas as pd
+import requests
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_api(*args, **kwargs):
+    """
+    Template for loading data from API
+    """
+    year_month = '2020-01'
+    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year_month}.parquet'
+
+    return pd.read_parquet(url)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
