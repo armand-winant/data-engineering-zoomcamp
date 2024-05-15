a1100da46457cf0604c6bc82195064db67179958
Armand <armandwinant@googlemail.com>
2024-05-15 09:27:05 +0000

    create model for staging taxi trips data

---
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -0,0 +1,38 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_tripdata') }}
+
+),
+
+renamed as (
+
+    select
+        vendor_id,
+        lpep_pickup_datetime,
+        lpep_dropoff_datetime,
+        store_and_fwd_flag,
+        ratecode_id,
+        pu_location_id,
+        do_location_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        payment_type,
+        trip_type,
+        congestion_surcharge,
+        __index_level_0__
+
+    from source
+
+)
+
+select * from renamed
